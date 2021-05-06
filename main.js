41c0cc19a18388733f472ef2b024a2984cb29459
Luke N. Arnold <lukenarnold@gmail.com>
2021-05-06 14:54:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//import { Handlebars, HandlebarsConfig } from 'https://deno.land/x/handlebars/mod.ts';
+import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
 import { parse } from "https://deno.land/std@0.95.0/flags/mod.ts"
 import * as Style from "https://deno.land/std/fmt/colors.ts"
 
@@ -29,12 +29,21 @@ let date = new Date();
 
 // Prompt the user for invoice field data
 let invoice_data = {
-    'hint-1': console.log(Style.bold(Style.underline('Invoice Details:'))),
-    invoice_number: prompt('    Invoice #:'),
-    creation_date: prompt('    Date:', datePretty(date.getMonth(), date.getDate(), date.getFullYear())),
-    due_date: prompt('    Due date:', datePretty((date.getMonth()+1)%12, date.getDate(), date.getFullYear()+Math.floor(date.getMonth()/12))),
-    'hint-2': console.log('\nList items in the format: Spaceship, Painting Lesson, Tech Support'), 
-    items: prompt('    Items:')
+    'general-section': console.log(Style.bold(Style.underline('General Details:'))),
+        invoice_number: prompt('    Invoice #:'),
+        creation_date: prompt('    Date:', datePretty(date.getMonth(), date.getDate(), date.getFullYear())),
+        due_date: prompt('    Due date:', datePretty((date.getMonth()+1)%12, date.getDate(), date.getFullYear()+Math.floor(date.getMonth()/12))),
+
+    'bill-to': console.log(Style.bold(Style.underline('\nBilling:'))),
+        customer_name: prompt('    Customer Name:'),
+        company: prompt('    Company:'),
+        billing_address: prompt('    Billing Address:'),
+        billing_country: prompt('    Billing Country:'),
+        billing_city: prompt('    Billing City:'),
+    
+    'items-section': console.log(Style.bold(Style.underline('\nItem Ledger:'))),
+        'items-section': console.log(Style.italic('List items in the format: Rocket Design, Painting Lesson, Tech Support')), 
+        items: prompt('    Items:')
 }
 
 // Remove empty properties used for console.log
@@ -43,6 +52,6 @@ invoice_data.removeUndefinedProperties();
 // Convert items string array
 invoice_data.items = invoice_data.items.split(',');
 // Cleanup stray whitespace
-console.log(invoice_data.items.forEach( (e, index) => { invoice_data.items[index] = e.trim() } ))
+invoice_data.items.forEach( (e, index) => { invoice_data.items[index] = e.trim() } )
 
 console.log(invoice_data)
